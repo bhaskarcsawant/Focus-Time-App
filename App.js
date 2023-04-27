a6e7f6d7ee265662bde1737c2a9a0d8970942491
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-04-27 20:11:15 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import  {useState} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {Focus} from './src/features/focus/Focus'
+import Constants from 'expo-constants';
+
+
+
+
+export default function App() {
+
+  const [focusSubject, setFocusSubject] = useState(null)
+  return (
+    <View style={styles.container}>
+    {focusSubject ? null : <Focus />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+});
