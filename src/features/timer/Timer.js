13af3d00d10f9462eb4ca4772d4200c460b6178a
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-04-29 20:26:53 +0530

    added start stop functionality

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,21 +1,28 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 import { FontSize, Padding } from "../../utils/sizes";
 import { CountDown } from "../../components/CountDown";
 
 export const Timer = ({ subject }) => {
+  const [isStarted, setIsStarted] = useState(false);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Focusing on :</Text>
       <Text style={styles.subject}>{subject}</Text>
-      <CountDown min={"20"} />
+      <CountDown min={20} isPaused={!isStarted} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setIsStarted(!isStarted)}
+      >
+        <Text style={styles.StartTitle}>{isStarted ? "Stop" : "Start"}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.5,
+    flex: 1,
     padding: Padding.lg,
     alignItems: "center",
   },
@@ -23,15 +30,23 @@ const styles = StyleSheet.create({
     fontSize: FontSize.lg,
     fontWeight: "400",
   },
+  StartTitle: {
+    fontSize: FontSize.xl,
+    fontWeight: "600",
+  },
   subject: {
     fontSize: FontSize.xl,
     fontWeight: "500",
     color: "green",
   },
-  timer: {
-    fontSize: FontSize.xxl,
-    fontWeight: "600",
-    color: "red",
-    marginTop: 50,
+  button: {
+    borderRadius: 100,
+    borderColor: "black",
+    borderWidth: 3,
+    width: 200,
+    height: 200,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 120,
   },
 });
